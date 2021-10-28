1902a3fcd3bab27897b43a3bb716f73a3cd94272
Lily Huang <l613426210@gmail.com>
2021-10-27 23:40:16 -0600

    upload

---
+++ week5/Story.py
@@ -0,0 +1,57 @@
+'''
+File: Story
+Authour: Lily Huang
+Time:09/23/2021
+'''
+
+print("Have you watched '3 IDIOTS' Indian comedy-drama film?")
+print("In the movie, there is a scene that Rancho change some of words in his speech without telling Chatur.") 
+print("Chatur menmories the whole speech without notice and understanding, either.")
+print("Let's do something just like Rancho did in the movie")
+print("Please filling some words below accroding to the questions.")
+
+verb = input("What kind of verb you want to use? The verb that you like to do it. ")
+noun = input("What kind of noun you want to use? Something is noun and you like it. ")
+place = input("Which place you want to praise? ")
+num = input("What is your favorite number?")
+person1 = input("Who do you want to say to in the speech? ")
+position = input("What is this person's position? ")
+pronoun1 = input("Is him, her, them, it? ")
+person2 = input("What do you want to be mention in the speech? Can be identity, thing, or anything that is noun.")
+print("Thank you for filling out the words. Now, it is the time we listen to Chatur Ramalingam aka 'Silencer' speech! ")
+
+print()
+print("Chatur Ramalingam aka Silencer: ")
+print(f"Today, if {place.capitalize()} is touching sky-high limits, then the credit goes to only one person: {person1.upper()}! Give him a hand!") 
+print(f"For the past {num} years in {place.capitalize()}, {person1.capitalize()} has continuously committed {verb}. upon {verb}.")
+
+print()
+print("Chatur Ramalingam aka Silencer continue speech: ")
+print(f"I hope it continues to do so. We often wondered how a person in {pronoun1} lifetime can do these many {verb}. ")
+print(f"With this extreme self-discipline, {person1.capitalize()} made this capable. Correct usage of time, complete utilization of the bell. ")
+print(f"Somebody learn from {pronoun1}. Learn from {pronoun1}. Learn from {pronoun1}!")
+print(f"Today, we all {person2} are here. Tomorrow, we'll spread across so many countries. I promise you all, whichever country we are in,")
+print(f"there we'll {verb}! We'll bring glory to the name of {place.capitalize()}! We'll show everyone the ability to {verb} that {person2} over here have.")
+print(f"No other {person2}  across the globe has it! No other {person2}! No other {person2}!")
+print(f"Respected {position}, {person1}. You have given the thing this {place} needs the most:...")
+print()
+print(f"It is {noun.upper()}!")
+print()
+print(f"Chatur Ramalingam aka 'Silencer': Everyone has {noun}.Everyone keeps it hidden. Nobody ever gives it willingly!")
+print(f"You have given your {noun} to the hands of this {place}. Now, let us see how {pronoun1} makes use of it.")
+print(f"Let me recite a shloka for this golden occasion. Your Majesty, thou art great! Accept this humble offering!")
+
+print()
+
+print()
+print("The End")
+print("HAHAHAHA. It looks kind of odd but it is the whole point! Hope you will like it or laught.")
+print("\U0001F923 \U0001F923 \U0001F923 \U0001F923 \U0001F923 Smile up!")
+
+import datetime
+x = datetime.datetime.now()
+print(x)
+print()
+
+
+
