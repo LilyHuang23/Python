2ca568b1d394f9317ec97c8c934db4601e7bbac6
Lily Huang <86539201+LilyHuang23@users.noreply.github.com>
2021-10-29 10:19:28 -0600

    Upload

---
+++ Week7/cse110_images/A_Week7.py
@@ -0,0 +1,23 @@
+'''
+file: Week 7 assignment
+
+'''
+from PIL import Image
+print("correctly")
+
+image_original = Image.open("beach.jpg")
+width = 300
+height = 300
+width, height = image_original.size
+
+
+pixels_original = image_original.load()
+r, g, b =pixels_original[100, 200]
+print(r, g, b)
+pixels_original[100,195] = (0, 0, 0)
+pixels_original[100,196] = (0, 0, 0)
+pixels_original[100,197] = (0, 0, 0)
+pixels_original[100,198] = (0, 0, 0)
+pixels_original[100,199] = (0, 0, 0)
+
+image_original.save("beach_with_black_pixels.jpg")
