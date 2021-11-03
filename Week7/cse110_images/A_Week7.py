199c1084e2b17848ffe42ada67ace50ac4faca90
Lily Huang <86539201+LilyHuang23@users.noreply.github.com>
2021-11-03 11:05:15 -0600

    update

--- Week7/cse110_images/A_Week7.py
+++ Week7/cse110_images/A_Week7.py
@@ -5,13 +5,38 @@ from PIL import Image
 print("correctly")
 
 image_original = Image.open("beach.jpg")
-width = 300
-height = 300
+image_green = Image.open("penguin.jpg")
+image_new = Image.new("RGB", image_green.size)
+
 width, height = image_original.size
 
 
 pixels_original = image_original.load()
-r, g, b =pixels_original[100, 200]
+pixels_green = image_green.load()
+pixels_new = image_new.load()
+
+print(pixels_green[50, 50])
+print(pixels_green[750, 50])
+print(pixels_green[50, 550])
+print(pixels_green[750, 550])
+
+for y in range(height):
+    for x in range(width):
+        r, g, b = pixels_green[x,y]
+        if r<150 and g>100 and b<100:
+            pixels_new[x,y] = pixels_original[x,y]
+        else:
+            pixels_new[x,y] = pixels_green[x,y]
+image_new.show()
+
+
+
+
+
+
+
+
+r, g, b = pixels_original[100, 200]
 print(r, g, b)
 pixels_original[100,195] = (0, 0, 0)
 pixels_original[100,196] = (0, 0, 0)
