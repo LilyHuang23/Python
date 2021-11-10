6ed51581a5f9df41e135b5cc02b79137029d732b
Lily Huang <86539201+LilyHuang23@users.noreply.github.com>
2021-11-10 11:14:14 -0700

    Update A_Week10.py

--- Week10/A_Week10.py
+++ Week10/A_Week10.py
@@ -1,11 +1,13 @@
 '''
 
 '''
+print("Enter a list of numbers, type 'end' when finished.")
 
 item_list =[]
 item = ""
 
 while item != "end":
-    item_add = str(input("What item would you like to add?\n"))
+    item = str(input("What item would you like to add?\n"))
+    item_list.append(item)
     price_item = float(input("What is the price of 'socks'?\n"))
 print(f"{item_add} has been added to the cart.")
