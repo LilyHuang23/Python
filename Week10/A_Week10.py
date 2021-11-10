6c7aa200c0c85bc7b98b815a284389cddb3aa46b
Lily Huang <86539201+LilyHuang23@users.noreply.github.com>
2021-11-10 11:09:34 -0700

    upload

---
+++ Week10/A_Week10.py
@@ -0,0 +1,11 @@
+'''
+
+'''
+
+item_list =[]
+item = ""
+
+while item != "end":
+    item_add = str(input("What item would you like to add?\n"))
+    price_item = float(input("What is the price of 'socks'?\n"))
+print(f"{item_add} has been added to the cart.")
--- Week9/Week9.py
+++ Week9/Week9.py
@@ -1,7 +1,7 @@
 '''
 file: Week 9 inclass & team activity
 Topic: list
-'''
+
 #pre class
 #variable = [] == variable = list()
 clients = []
@@ -39,3 +39,60 @@ for tip_amount in tips:
 print(f"The total is: {running_total:.2f}")
 
 #inclass
+# A list is a COLLECTION!
+# Creating a list variable -> good_name = [ ]
+# create an empty list to hold ages
+ages = []
+# create a list of names
+name =[lily, andy, chloe]
+# add an age to the age list and a name to the name list
+ages.append(12)
+print('name: ', name, '\nages', ages)
+ages.append(12)
+ages.append(12)
+ages.append(12)
+ages.append(12)
+ages.extend([23,33,21,12])
+print('name: ', name, '\nages', ages)
+
+
+remember that for loops are good at going through collections
+for item in collection:
+    #do something each time we get an item
+
+# use a for loop to go through the lists and print each item
+for age in ages:
+    print(age)
+
+
+we can access each item in the list through an index
+lists are stored in memory in adjacent memory buckets
+                         __________________________
+     ages -------------> | 10 | 12 | 23 | 34 | 17 | 
+                         --------------------------
+                            0    1    2    3    4     index   
+
+# print just the second item in each list
+print(ages[1])
+print(name[1])
+# use a for loop to go through the ages list and
+# add all the ages together for a total of all ages
+'''
+#team activty
+
+print("Enter a list of numbers, type 0 when finished.")
+number_list = []
+number = ""
+while number != 0:
+    number = int(input("Enter a number: "))
+    number_list.append(number)
+count = sum(number_list) 
+print("Sum: ", count)
+avg = count/len(number_list)
+print("Average: ", avg)
+print("The smallest positive number is: ",(min([number for number in number_list if number > 0])))
+print("The largest number: ",max(number_list))
+number_list.sort()
+print("The sorted list is: ",number_list)
+
+#comprehension
