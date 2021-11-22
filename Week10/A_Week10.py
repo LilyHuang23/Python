8674453797727e2aed7e7357878900b1fa61ce25
Lily Huang <86539201+LilyHuang23@users.noreply.github.com>
2021-11-21 20:54:58 -0700

    UPLOAD

--- Week10/A_Week10.py
+++ Week10/A_Week10.py
@@ -1,15 +1,44 @@
 '''
-
+File: W10 Assignment
 '''
-print("Enter a list of numbers, type 'end' when finished.")
+# print("Enter a list of numbers, type 'end' when finished.")
 
 item_list =[]
 item = ""
+price = ""
 price_item =[]
+# price_item = float(input("What is the price of 'socks'?\n"))
+#         price_item.append(price)
+#         item_list.append(item)
+ 
+print("Welcome to the Shopping Cart Program! ")
 
-while item != "end":
-    item = str(input("What item would you like to add?\n"))
-    price_item = float(input("What is the price of 'socks'?\n"))
-    item_list.append(item)
+action = "" 
 
-print(f"{item} has been added to the cart.")
+while action != "5":
+    print("Please input one of the following: ")
+    print("1. Add item\n2. View cart\n3. Remove item\n4. Compute total\n5. Quit")
+    action = input("Please enter an action: ")
+    if action == "1":
+        item = str(input("What item would you like to add?\n"))
+        price= float(input(f"What is the price of {item}?\n"))
+        price_item.append(price)
+        item_list.append(item) 
+        print(f"{item} has been added to the cart.")
+    elif action == "2":
+        print("The contents of the shopping cart are: ")
+        for i in range(len(item_list)):
+            print(f"{i+1}.{item_list[i]} - ${price_item[i]:.2f}")
+    elif action == "3":
+        remove = int(input("Which item number would you like to remove? "))
+        item_list.pop(remove-1)
+        price_item.pop(remove-1)
+        print("Item remove")
+    elif action == "4":
+        total = sum(price_item)
+        print(f"The total price of the items in the shopping cart is ${total:.2f}")
+    else:
+        print("thank you!")
+
+
+
--- Week10/Week10.py
+++ Week10/Week10.py
@@ -1,4 +1,116 @@
 '''
 File: Week 10 in class & Team activity
-Topic:
+Topic: Index
+
+
+nums = []
+nums = [1,3,5,7]
+print(f"The thrid number is: {nums[2]}")#using the index
+
+length = len(nums)
+print(length)
+
+#loop through 
+for item in nums:
+    print(item)
+#loop through a list using index(and range)
+print("\nprint with index")
+for index in range(len(nums)):
+    print(nums[index])
+    #print(f"{index=} {nums[index]=}")
+
+#user friendly
+for index in range(len(nums)):
+    #print(nums[index])
+    print(f"{index+1}... {nums[index]}")
+#index for two list
+names=["lily","andy","chloe"]
+ages = [10,23,25]
+for index in range(len(names)):
+    #print(nums[index])
+    print(f"{index+1}... {names[index]}, age:{ages[index]}")
+
+#input the variable
+print("\nbefore change 2nd item(index1):",ages)
+ages[1] = 19
+
+print("after change:",ages)
+# ask the user which item they want to replace
+# if I used user friendly numbers to display, 
+# I need to -1 to get the correct index 
+for i in range(len(ages)):
+    print(i+1, ages[1])
+index = int(input("which age do you want to change:"))
+
+# show the list again to show the new age is in the list
+# print('\naltered list:', ages)
+
+
+
+# remove an item from the nums list with pop()
+nums = [1,5,7,9]  # initialized list
+print('\nbefore popping from nums')
+print(nums)
+popped_item = nums.pop(1)#index number
+print("\nafter popping from nums")
+print(nums)
+# print('\nafter popping from nums')
+
+# print the item removed (popped)
+print(popped_item)
 '''
+#team
+print("Enter the names and balances of bank accounts (type: quit when done)")
+name_list = []
+num_list = []
+
+name = ""
+num = ""
+
+while name !="end":
+    name = str(input("What is the name of this account?" ))
+    if name !="end":
+        name_list.append(name)
+        num = float(input("What is the balance? $"))
+        num_list.append(num)
+
+print("Account Information: ")
+for i in range(len(name_list)):
+    print(f"{i+1},{name_list[i]},{num_list[i]}")
+
+
+Total = sum(num_list)
+print(f"Total: ${Total}")
+
+Average = Total/len(num_list)
+print(f"Average: ${Average}")
+
+highest = max(num_list)
+print(f"The highest balance: ${highest}")
+
+
+change = str(input("Do you want to update an account? yes/no\n"))
+
+change = ""
+
+while change =="yes":
+    update = float(input("What account index do you want to update? "))
+    new_num = float(input("What is the new amount? \n$ "))
+else:
+    print("Sweet!")
+
+
+
+
+
+
+
+# print()
+
+# BONUS: insert a value at index 1 (may confuse some)
+# val = (input('Enter a value to insert at index 1: '))
+# nums.insert(1, val)
+# print('list with value inserted at index 1: ', nums)
+# print()
+
+
